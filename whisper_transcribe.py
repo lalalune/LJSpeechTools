77203fc65b83882d6b88252c221b8b812de4321f
michael <mcaughey001@gmail.com>
2024-02-06 12:35:51 -0500

    added messaging for transcription

--- whisper_transcribe.py
+++ whisper_transcribe.py
@@ -8,7 +8,9 @@ class Whisper_Transcriber():
     model = None
 
     def init(self, model_name = "large-v3"):
+        print(f"Loading Open AI Whisper model {model_name}")
         self.model = whisper.load_model(model_name)
+        print("  complete")
 
     def transcribe(self, audio_file_name):
         if self.model == None:
